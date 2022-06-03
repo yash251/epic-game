86d84905d0695a4c0163da75dc906c6c49471786
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-03 13:24:25 +0530

    test for rinkeby

---
+++ scripts/deploy.js
@@ -0,0 +1,48 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(
+      ["Shiva", "Ganesha", "Hanuman"],
+      ["https://i.imgur.com/uuhHEbk.jpeg",
+      "https://i.imgur.com/hBFmGg5.jpeg",
+      "https://i.imgur.com/Vy1H7TH.jpeg"],
+      [500, 450, 475],
+      [300, 270, 250]
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+
+    let txn;
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #1");
+
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #2");
+
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #3");
+
+    txn = await gameContract.mintCharacterNFT(0);
+    await txn.wait();
+    console.log("Minted NFT #4");
+
+    console.log("Done deploying and minting!");
+
+    // Get the value of NFT's URI
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI : ", returnedTokenUri);
+};
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+};
+  
+runMain();
