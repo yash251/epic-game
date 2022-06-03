a1ba2fa83c3d07b49b0a3fcf748c209fd86cd556
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-03 10:23:14 +0530

    using the ERC721 to mint

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -2,9 +2,17 @@
 
 pragma solidity ^0.8.0;
 
+// NFT contract to inherit from
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions Openeppelin provides
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+// Inheriting from ERC721, which is std. NFT contract
+contract MyEpicGame is ERC721 {
     // hold our character attributes in a struct
     struct CharacterAttributes {
         uint256 characterIndex;
@@ -14,9 +22,21 @@ contract MyEpicGame {
         uint256 maxHp;
         uint256 attackDamage;
     }
+
+    // For tokenId
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     // A lil array to help us hold the default data for our characters.
     // This will be helpful when we mint new characters and need to know
     CharacterAttributes[] defaultCharacters;
+
+    // A mapping from NFT's tokenId => that NFT's attributes
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => the NFTs tokenId. Gives us an easy way
+    // to store the owner of the NFT and reference it later
+    mapping(address => uint256) public nftHolders;
 
     constructor(
         string[] memory characterNames,
@@ -24,6 +44,7 @@ contract MyEpicGame {
         uint[] memory characterHp,
         uint[] memory characterAttackDmg
     ) 
+        ERC721("Heroes", "HERO")
     {   
         // Loop through all the characters, and save their values in our contract so
         // we can use them later when we mint our NFTs.
@@ -40,5 +61,34 @@ contract MyEpicGame {
             CharacterAttributes memory c = defaultCharacters[i];
             console.log("Done initializing %s with HP %s, img %s", c.name, c.hp, c.imageURI);
         }
+
+        // Increment _tokenIds here so that my first NFT has an ID of 1
+        _tokenIds.increment();
+    }
+    // Users would be able to hit this function and get their NFT based on the
+    // characterId they send in!
+    function mintCharacterNFT(uint _characterIndex) external {
+        // Get the current tokenId
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId); // inbuilt fxn
+
+        // Map the tokenId => their character attributes
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex : _characterIndex,
+            name : defaultCharacters[_characterIndex].name,
+            imageURI : defaultCharacters[_characterIndex].imageURI,
+            hp : defaultCharacters[_characterIndex].hp,
+            maxHp : defaultCharacters[_characterIndex].maxHp,
+            attackDamage : defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log("Minted NFT with tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+        // Way to see who owns which NFT
+        nftHolders[msg.sender] = newItemId;
+
+        // Increment the tokenId for the next person that uses it
+        _tokenIds.increment();
     }
 }
