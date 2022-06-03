a519fde1b6d8150d317e862f08ea5a7e90d35f53
Yash Jagtap <jagtapyash2512@gmail.com>
2022-06-03 09:30:39 +0530

    initialize attr and use struct, array

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,40 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("This is a game contract!");
+    // hold our character attributes in a struct
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+    // A lil array to help us hold the default data for our characters.
+    // This will be helpful when we mint new characters and need to know
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg
+    ) 
+    {   
+        // Loop through all the characters, and save their values in our contract so
+        // we can use them later when we mint our NFTs.
+        for (uint i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(CharacterAttributes({
+                characterIndex : i,
+                name : characterNames[i],
+                imageURI : characterImageURIs[i],
+                hp : characterHp[i],
+                maxHp : characterHp[i],
+                attackDamage : characterAttackDmg[i]
+            }));
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done initializing %s with HP %s, img %s", c.name, c.hp, c.imageURI);
+        }
     }
 }
